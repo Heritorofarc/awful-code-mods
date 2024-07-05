fe0b331bffe5ea23305cb6770e1924f7b6231ff8
Heritorofarc <88900822+Heritorofarc@users.noreply.github.com>
2024-07-05 03:51:34 -0400

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -7,9 +7,9 @@ use engage::mess::Mess;
 #[skyline::hook(offset=0x02160ac0)]
 //#[unity::hook("App", "HelpParamSetter", "SetBattleInfo")] <<< another way to hook, uncomment if you want to use this instead.
 //Function uses 6 parameters. (what these [vvvv] are called) when hooking into a vanilla function [^^^^] make sure you match its parameters, visible in Ghidra.
-pub fn set_battle_info_hook(cock: u64, balls: u64, unit: &Unit, text_type: i32, text: &Il2CppString, method_info: OptionalMethod){
+pub fn set_battle_info_hook(this: u64, frame: u64, unit: &Unit, text_type: i32, text: &Il2CppString, method_info: OptionalMethod){
     //this calls the original function to run, which displays the tooltip text as normal.
-    call_original!(cock, balls, unit, text_type, text, method_info);
+    call_original!(this, frame, unit, text_type, text, method_info);
     //if not part of blue party or in somniel (eg, an enemy unit), end code -- displays text like vanilla as nothing gets added
     if unit.force.unwrap().force_type != 0 && unit.force.unwrap().force_type != 3 {
         return;
@@ -40,7 +40,7 @@ pub fn set_battle_info_hook(cock: u64, balls: u64, unit: &Unit, text_type: i32, 
             Mess::get("MID_GAMESTART_GROWMODE_SELECT_TITLE").get_string().unwrap() , 
             grow);
         //when [if grow > 0] is fulfilled, modify the text behind the scenes and then output it with the additions specified (growth display)
-        call_original!(cock, balls, unit, text_type, new_text.into(), method_info); 
+        call_original!(this, frame, unit, text_type, new_text.into(), method_info); 
     }
 }
 #[skyline::main(name = "hooks")]
